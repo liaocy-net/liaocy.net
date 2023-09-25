765c477ae3159b64291a4e811b3206d0cd04ea6a
liaocyintl <liaocyintl@hotmail.com>
2023-09-25 13:31:08 +0000

    init. gtm

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,6 +86,9 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleTagManager: {
+          containerId: 'GTM-TFMD8C7D',
+        },
       }),
     ],
   ],
